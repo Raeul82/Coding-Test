68e74134a3eb4ca74389f551b380f7e9e7d125dc
Raeul <74648478+Raeul82@users.noreply.github.com>
2025-04-30 16:11:27 +0900

    Title: 물의 상태, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250430/물의 상태/state-of-water.java
+++ 250430/물의 상태/state-of-water.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.print("ice");
         } else if (temp > 100) {
             System.out.print("vapor");
-        } else if (temp > 0 && temp <100) {
+        } else if ((temp > 0) && (temp < 100)) {
             System.out.print("water");
         }
         // Please write your code here.
