f45cb5a0d8a335d719d5e70094a24461dd9805e7
Raeul <74648478+Raeul82@users.noreply.github.com>
2025-05-02 15:41:58 +0900

    Title: 점수 비교, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250502/점수 비교/score-comparison.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEnglish = sc.nextInt();
+
+        int bMath = sc.nextInt();
+        int bEnglish = sc.nextInt();
+
+        if(aMath > bMath && aEnglish > bEnglish) {
+            System.out.print("1");
+        }
+        else {
+            System.out.print("0");
+        }
+
+    }
+}
