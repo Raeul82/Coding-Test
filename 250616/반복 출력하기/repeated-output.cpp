dc858c68777cdea050eda4174b6659aabbff59be
Raeul <74648478+Raeul82@users.noreply.github.com>
2025-06-16 10:46:54 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250616/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void Print(int N) {
+    for(int i=0; i<N; i++)
+        cout << "12345^&*()_" << endl;
+}
+
+int main() {
+    cin >> N;
+
+    Print(N);
+
+    return 0;
+}
