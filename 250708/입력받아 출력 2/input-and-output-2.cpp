625b511165e0a20bbfc638908815368439109a20
Raeul <74648478+Raeul82@users.noreply.github.com>
2025-07-08 14:15:03 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int front, rear;
+
+    cin >> front;
+    cin.get();
+    cin >> rear;
+
+    cout << front << rear;
+    
+    return 0;
+}
