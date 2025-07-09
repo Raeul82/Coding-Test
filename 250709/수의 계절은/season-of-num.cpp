359dcabebc7cea5da6278c4f66d78d014e087979
Raeul <74648478+Raeul82@users.noreply.github.com>
2025-07-09 12:48:37 +0900

    Title: 수의 계절은, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250709/수의 계절은/season-of-num.cpp
+++ 250709/수의 계절은/season-of-num.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     int m;
     cin >> m;
-    if (m>=3 || m<=5)
+    if (m>=3 && m<=5)
         cout << "Spring";
-    else if (m>=6 || m<=8)
+    else if (m>=6 && m<=8)
         cout << "Summer";
-    else if (m>=9 || m<=11)
+    else if (m>=9 && m<=11)
         cout << "Fall";
     else if (m==12 || m<=2)
         cout << "Winter";
